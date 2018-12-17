bc473deee679bc38efbe85a9bf8caf41b28e712c
Mathieu Lemire <mathieu.lemire@sickkids.ca>
2018-12-17 11:00:30 -0500

    changed pca procedure

--- qc_2.sh
+++ qc_2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-module load plink/1.07
+module load plink/1.90b3x
 module load R/3.5.1
 
 alias plink='plink --noweb'
@@ -19,29 +19,44 @@ scriptdir=/hpf/projects/arnold/users/mlemire/scripts/qc/genotypingarrays
 
 
 
-awk '{print $1":"$4,$5,$6}' $kgdir/snpsForPCA.bim  | sort -k 1,1 > ${tmpfile}.1kg
-awk '{print $1":"$4,$2,$5,$6}' $dir/${prefix}.bim  | sort -k 1,1 > ${tmpfile}.bim
+awk '{print $2,$5,$6}' $kgdir/indep.bim  | sort -k 1,1 > ${tmpfile}.1kg
+awk '$1<=22 {print $2,$5,$6}' $dir/${prefix}.bim  | sort -k 1,1 > ${tmpfile}.bim
 
 # excluding A/T and C/G SNPs 
-# common based on chr:pos:a1:a2
+# common based on chr:pos:a1:a2 
 join -1 1 -2 1 ${tmpfile}.bim ${tmpfile}.1kg |\
- awk '( ($3==$5 && $4==$6 ) || ($4==$5 && $3==$6 ) ) && $3$4!="AT" && $3$4!="TA" && $3$4!="CG" && $3$4 !="GC" {print $2}'  > ${tmpfile}.common 
+ awk 'BEGIN{s["A"]="T";s["C"]="G";s["G"]="C";s["T"]="A" }
+      (($2==$4 && $3==$5 ) || ($3==$4 && $2==$5 ) || ($2==s[$4] && $3==s[$5] ) || ($3==s[$4] && $2==s[$5] )) && 
+      $2$3!="AT" && $2$3!="TA" && $2$3!="CG" && $2$3 !="GC" {print $1}'  > ${tmpfile}.common 
 
-# merging
+
 # extracting first in one dataset because plink complains about strand for SNPs not in the extract list 
-# also recoding the SNP name 
+# also recoding the SNP name based on chr:pos
+# duplicates should have been flagged in the exclude file 
+
 plink --bfile $dir/$prefix  --extract  ${tmpfile}.common --exclude ${prefix}.exclude.txt  --make-bed --out ${tmpfile}_${prefix} 
-awk '{$5<$6?al=$5":"$6:al=$6":"$5}{$2=$1":"$4":"al; $3=0}{print}' ${tmpfile}_${prefix}.bim | sed 's/ /\t/g'  > ${tmpfile}.tmp
-\mv  ${tmpfile}.tmp  ${tmpfile}_${prefix}.bim 
+#awk '{$5<$6?al=$5":"$6:al=$6":"$5}{$2=$1":"$4; $3=0}{print}' ${tmpfile}_${prefix}.bim | sed 's/ /\t/g'  > ${tmpfile}.tmp
+#\mv  ${tmpfile}.tmp  ${tmpfile}_${prefix}.bim 
 
-# need to recode snp name based on position
-awk '{print $2}' ${tmpfile}_${prefix}.bim > ${tmpfile}.common 
-awk '{$5<$6?al=$5":"$6:al=$6":"$5}{$2=$1":"$4":"al; $3=0}{print}'  $kgdir/indep.bim > ${tmpfile}_indep.bim 
-plink --bed $kgdir/indep.bed --fam $kgdir/indep.fam --bim  ${tmpfile}_indep.bim    --extract  ${tmpfile}.common --make-bed --out ${tmpfile}_1kg  
 
-plink --bfile  ${tmpfile}_${prefix} --bmerge ${tmpfile}_1kg.bed ${tmpfile}_1kg.bim  ${tmpfile}_1kg.fam --exclude ${prefix}.exclude.txt --recodeA --out ${tmpfile} 
+# pruning LD 
+plink --bfile  ${tmpfile}_${prefix} --maf 0.05 --indep-pairwise 1500 100 0.1 --out ${tmpfile}_${prefix}
+plink --bfile  ${tmpfile}_${prefix} --extract  ${tmpfile}_${prefix}.prune.in --make-bed --out ${tmpfile}_${prefix}_pruned 
 
-R --no-save --args ${tmpfile}.raw ${prefix}.1kgpca < $scriptdir/qc_pca.r
+# extracting in 1kg. recoding snps names first 
+# awk '{$5<$6?al=$5":"$6:al=$6":"$5}{$2=$1":"$4; $3=0}{print}'  $kgdir/indep.bim > ${tmpfile}_indep.bim 
+plink --bed $kgdir/indep.bed --fam $kgdir/indep.fam --bim $kgdir/indep.bim    --extract ${tmpfile}_${prefix}.prune.in  --make-bed --out ${tmpfile}_1kg  
+
+plink --bfile  ${tmpfile}_${prefix} --bmerge ${tmpfile}_1kg.bed ${tmpfile}_1kg.bim  ${tmpfile}_1kg.fam --exclude ${prefix}.exclude.txt --make-bed  --out ${tmpfile}
+
+# first round of flipping 
+plink --bed $kgdir/indep.bed --fam $kgdir/indep.fam --bim $kgdir/indep.bim    --extract ${tmpfile}_${prefix}.prune.in --flip  ${tmpfile}-merge.missnp --make-bed --out ${tmpfile}_1kg  
+plink --bfile  ${tmpfile}_${prefix}_pruned --bmerge ${tmpfile}_1kg.bed ${tmpfile}_1kg.bim  ${tmpfile}_1kg.fam --exclude ${prefix}.exclude.txt --make-bed  --out ${tmpfile}_merge
+
+plink --bfile ${tmpfile}_merge --pca --out ${prefix}.qc_pca 
+
+
+R --no-save --args ${prefix}.qc_pca.eigenvec  ${tmpfile}_merge.fam  ${prefix}.1kgpca < $scriptdir/qc_pca.r
 
 \rm ${tmpfile}* 
 
--- qc_pca.r
+++ qc_pca.r
@@ -1,73 +1,76 @@
 ARGS<- commandArgs( TRUE )
 
-# input is a raw file produced with --recodeA option of plink 
-input<- ARGS[1]
-output.prefix  <-ARGS[2]
+# input.prefix is a raw file produced with --recodeA option of plink 
+input.pca <- ARGS[1]
+input.fam<- ARGS[2] 
+output.prefix  <-ARGS[3]
 
 kgsamples <- read.table("/hpf/projects/arnold/references/www.tcag.ca/documents/tools/sampleTable.txt", header=T )
 pop<- kgsamples[, c("ID","Continent")]
 names(pop)[1]<-"IID"
 
-gen <-read.table( input , head=T )
-ped<- data.frame( gen[,1:6], o=1:nrow( gen ) )
-ped<- merge( ped, pop, all.x=T  ) 
-ped<- ped[ order(ped$o), -7 ] 
+gen <-read.table( input.fam , head=F )
+ped<- data.frame( gen[,1:2], o=1:nrow( gen ) )
+names( ped )[1:2]<-c("FID","IID" )
+ped<- merge( ped, pop, all.x=T , by="IID"  ) 
+ped<- ped[ order(ped$o), -3 ] 
+ped$ID<- paste( ped$FID,ped$IID, sep=":" )
+ped<- ped[ , -(1:2)]
 
-gen<- gen[,-(1:6)] 
+pca<- read.table(input.pca, header=F )
+names( pca )[1:5]<-c("FID","IID", "PC1","PC2","PC3" )
+pca$ID<- paste( pca$FID,pca$IID, sep=":" )
 
-impute<-function(v){
- sel<- is.na(v)
- v[sel]<- mean( v[!sel] )
- return( v )
-}
-
-gen.imputed<- apply( gen, 2, impute ) 
-
-system.time( pr<- prcomp( t( gen.imputed ), scale.=F, center=T  , retx=F ) )
-pr.ped<- list( pr, ped )
-save( pr.ped , file=paste( output.prefix,".prcomp.rda", sep="") ) 
-write.table(cbind(  pr.ped[[2]][,1:2], pr.ped[[1]]$rotation[,1:3] ) , file=paste( output.prefix,".prcomp.txt", sep="")  , quote=F, row=F, col=T ) 
+pca<- merge( ped, pca , by="ID" ) 
 
 
 
 # logical indicates which are the samples (ie not 1kg)
-samples<- is.na( ped$Continent )
+samples<- is.na( pca$Continent )
+
+
 
 # outliers wrt samples only 
 
+
+
 outliers<- 
-  apply( pr$rotation[ samples , 1:3] , 2, 
+  apply( pca[ samples ,c( "PC1","PC2","PC3" ) ] , 2, 
       function(v){ 
         bp<- boxplot(v, plot=F, range=3 )
         is.element( v, bp$out )
       }
   )
 
+
+
 outliers<- apply( outliers, 1, any ) 
 
-write.table( ped[ samples, c("FID","IID")][outliers,] , paste( output.prefix, ".outliers.txt" , sep="" ), quote=F, col=T, row=F )
+
+write.table( pca[ samples, c("FID","IID")][outliers,] , paste( output.prefix, ".outliers.txt" , sep="" ), quote=F, col=T, row=F )
 
 
-col<- as.integer( ped$Continent ) +1 
+col<- as.integer( pca$Continent ) +1 
 # black
 col[samples]<- 1
 
-pch<- rep( 21, nrow(ped) )
+pch<- rep( 21, nrow(pca) )
 # samples are bullets
-pch[ is.na( ped$Continent ) ]<-19 
+pch[ is.na( pca$Continent ) ]<-19 
 # outliers are circles with X in them  
 pch[samples][ outliers ]<- 13
 
 pdf( paste( output.prefix, ".pcaplot.pdf",sep=""),  width=6, height=18)
 par( mfrow=c(3,1) )
-plot( pr$rotation[,1:2] , col=col, xlab="PC1", ylab="PC2", pch=pch  ) 
-points(  pr$rotation[samples ,1:2] , col=col, pch=pch[samples]   ) 
-legend( x="bottomleft", legend=c("samples", lev<-levels(ped$Continent) ), pch=19, col=1:(1+length( lev )) )
-plot( pr$rotation[,c(1,3)] , col=col , xlab="PC1", ylab="PC3", pch=pch  ) 
-points(  pr$rotation[samples ,c(1,3)] , col=col, pch=pch[samples]  ) 
-plot( pr$rotation[,2:3] , col=col , xlab="PC2", ylab="PC3") 
-points(  pr$rotation[samples ,2:3] , col=col, pch=pch[samples]  ) 
+plot( pca[,c("PC1","PC2") ] , col=col, xlab="PC1", ylab="PC2", pch=pch  ) 
+points(  pca[samples ,c("PC1","PC2") ] , col=col, pch=pch[samples]   ) 
+legend( x="bottomleft", legend=c("samples", lev<-levels(pca$Continent) ), pch=19, col=1:(1+length( lev )) )
+plot( pca[,c("PC1","PC3")] , col=col , xlab="PC1", ylab="PC3", pch=pch  ) 
+points(  pca[samples ,c("PC1","PC3")] , col=col, pch=pch[samples]  ) 
+plot( pca[,c("PC2","PC3")] , col=col , xlab="PC2", ylab="PC3") 
+points(  pca[samples ,c("PC2","PC3") ] , col=col, pch=pch[samples]  ) 
 dev.off()
+
 
 
 
