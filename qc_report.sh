f254c768e01f288a1984c3876da2baf3a67302e8
Mathieu Lemire <mathieu.lemire@sickkids.ca>
2019-06-04 09:22:47 -0400

    first qc_report.sh script

---
+++ qc_report.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+prefix=$1 
+
+scriptdir=/hpf/projects/arnold/users/mlemire/scripts/qc/genotypingarrays
+
+today=`date`
+here=`pwd -P`
+
+imissrate=
+lmissrate=0.03
+
+
+echo "0        1         2         3         4         5         6         7"
+echo 1234567890123456789012345678901234567890123456789012345678901234567890
+echo "######################################################################" 
+echo "#" Report produced on $today 
+echo "#" $here
+echo "######################################################################" 
+echo 
+echo 1. SNP exclusion
+echo 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#############################################################################
+#   Copyright 2019 Mathieu Lemire
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#############################################################################
