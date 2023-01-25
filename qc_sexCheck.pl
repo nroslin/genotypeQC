1336c5aeaa21eec89d6d5fc66692c961ea28d570
Nicole M. Roslin <nroslin@sickkids.ca>
2023-01-25 10:05:05 -0500

    Trapped division by zero error on chrY

--- qc_sexCheck.pl
+++ qc_sexCheck.pl
@@ -3,12 +3,14 @@
 use strict;
 
 # Created 20 January 2022.
-# Last modified:  12 Jan 2023
+# Last modified:  25 Jan 2023
 
 # Read in output of bcftools for chr X and Y data.  Super annoying to try in R.
 
 my $inchrx = "$ARGV[0]_c23.bcfout";   #chrX, output of bcftools
 my $inchry = "$ARGV[0]_c24.imiss";   #chrY missingness from PLINK
+my $incrx = "$ARGV[0]_c23.imiss";   #chrX missingness from PLINK
+my $inhety = "$ARGV[0]_c24.bcfout";   #chrY het
 my $inped = "$ARGV[1]/$ARGV[2].fam";   #ped file with gender info
 my $output = "$ARGV[2]_sexCheck.txt";
 my ( %datahash, $ynsnp );
@@ -24,7 +26,12 @@ while (<CX>) {
   next if /^DEF/;
   next if /^SITE0/;
   my ( $xid, $xnsnp, $xnhet ) = (split)[1,2,6];
-  my $xhet = sprintf "%.4f", $xnhet / $xnsnp;
+  my $xhet;
+  if ( $xnsnp == 0 ) {
+	$xhet = 0;  #or should it be NA?  no calls on this chr
+	print "No valid SNPs on chrX for $xid\n";
+  }
+  else { $xhet = sprintf "%.4f", $xnhet / $xnsnp; }
   $datahash{$xid} = join "\t", $xnsnp, $xnhet, $xhet;
 }
 close CX;
@@ -44,16 +51,58 @@ while (<CY>) {
 }
 close CY;
 
+#chr X, get call rate
+#not sure yet how helpful this will be
+if ( -e $incrx ) {
+  open CRX, "$incrx" or die "Cannot open file $incrx:  $!";
+  while (<CRX>) {
+	chomp;
+	next if /FID/;
+	my ( $cfid, $ciid, $cnmiss, $cnsnp, $cfmiss ) = (split)[0,1,3,4,5];
+	my $xsample = join "_", $cfid, $ciid;
+	#my $xosnp = $cnsnp - $cnmiss;   #number of non-missing genotypes
+	my $xcr = sprintf "%.4f", 1 - $cfmiss;   #call rate
+	$datahash{$xsample} = join "\t", $datahash{$xsample}, $xcr;
+  }
+}
+
+#chrY het
+#id here is FID_IID
+open YHET, "$inhety" or die "Cannot open file $inhety:  $!";
+while (<YHET>) {
+  chomp;
+  next if /^#/;
+  next if /^CMD/;
+  next if /^DEF/;
+  next if /^SITE0/;
+  my ( $yhetid, $yhnsnp, $ynhet ) = (split)[1,2,6];
+  my $yhet;
+  if ( $yhnsnp == 0 ) {
+	$yhet = 0;  #should I make it NA instead?
+	print "No valid SNPs on chrY for $yhetid\n";
+  }
+  else { $yhet = sprintf "%.4f", $ynhet / $yhnsnp; }
+  $datahash{$yhetid} = join "\t", $datahash{$yhetid}, $yhet;
+}
+close YHET;
+
+
 #print everything out
+### assume that if have both or none of chrX call rate and chrY het
 open OUT, ">$output" or die "Cannot write to file $output:  $!";
-print OUT "FID\tIID\tPedSex\tChrXnSNP\tChrXnHet\tChrXhetRate\tChrYnSNP\tChrYnCalled\tChrYcr\n";
+print OUT "FID\tIID\tPedSex\tChrXnSNP\tChrXnHet\tChrXhetRate\tChrYnSNP\tChrYnCalled\tChrYcr\tChrXcr\tChrYhet\n";
 
 open PED, "$inped" or die "Cannot open file $inped:  $!";
 while (<PED>) {
   chomp;
   my ( $fid, $iid, $sex ) = (split)[0,1,4];
   my $label = join "_", $fid, $iid;
-  print OUT "$fid\t$iid\t$sex\t$datahash{$label}\n";
+### add NA if X chr call rate file not present
+  my @array = split $datahash{$label};
+  if ( scalar @array == 6 ) {   #if no chrX call rate or chrY het
+	print OUT "$fid\t$iid\t$sex\t$datahash{$label}\tNA\tNA\n";
+  }
+  else { print OUT "$fid\t$iid\t$sex\t$datahash{$label}\n"; }
 }
 close PED;
 close OUT;
--- qc_sexCheck.r
+++ qc_sexCheck.r
@@ -1,76 +1,131 @@
 # Created 20 January 2022.
-# Last modified:  16 Jan 2023
+# Last modified:  18 Jan 2023
 
 # Make some plots and do some sex inference.
+# Estimate how many of each sex chromosome a person carries (0, 1, 2, 
+# undetermined), then use this to estimate sex (male, female, unclear).  The
+# ones that are unclear can be examined for possible anomalies (eg. XXY).
+# Use fixed threshold to establish counts.
 
 infile<-commandArgs(trailingOnly=T)
 outfile<-paste0(infile, "_inferredSex.txt")
 
+
 x<-read.table(paste0(infile, "_sexCheck.txt"), header=T, as.is=T)
 
 #make some plots
-pdf(paste0(infile, "_sexCheck.pdf"), height=10, width=15)
-plot(x$ChrXhetRate, x$ChrYcr, main=paste(infile, "All samples"), pch=x$PedSex+1, 
+pdf(paste0(infile, "_sexCheck.pdf"), height=5, width=15)
+par(mfrow=c(1,3))
+#chr X het vs. chr Y call rate
+plot(x$ChrXhetRate, x$ChrYcr, main=paste(infile, ": All samples"), pch=x$PedSex+1, 
   col=x$PedSex+1, xlab="ChrX heterozygosity", ylab="ChrY call rate")
-par(mfcol=c(2,3))
+segments(0.10, 0.4, 0.3, 0.4, lty=2)
+segments(0.10, 0.4, 0.10, -1, lty=2)
+segments(0.3, 0.4, 0.3, -1, lty=2)
+segments(-1, 0.9, 0.05, 0.9, lty=2)
+segments(0.05, 0.9, 0.05, 1.1, lty=2)
 
-pu<-x[x$PedSex==0,]
-pm<-x[x$PedSex==1,]
-pf<-x[x$PedSex==2,]
+#chrX het vs. chr X call rate
+plot(x$ChrXhetRate, x$ChrXcr, main=paste(infile, ": All samples"), pch=x$PedSex+1, 
+  col=x$PedSex+1, xlab="ChrX heterozygosity", ylab="ChrX call rate")
 
-if ( nrow(pu) > 0 ) {
-  plot(sort(pu$ChrXhetRate), main="Ped unknown, chrX het", ylim=c(0,0.5))
-  plot(sort(pu$ChrYcr), main="Ped unknown, chrY CR", ylim=c(0,1))
-} else {
-  plot.new()
-  plot.new()
-}
-
-if ( nrow(pm) > 0 ) {
-  plot(sort(pm$ChrXhetRate), main="Ped male, chrX het", ylim=c(0,0.5))
-  plot(sort(pm$ChrYcr), main="Ped male, chrY CR", ylim=c(0,1))
-} else {
-   plot.new()
-   plot.new()
-}
-
-if ( nrow(pf) > 0 ) {
-  plot(sort(pf$ChrXhetRate), main="Ped female, chrX het", ylim=c(0,0.5))
-  plot(sort(pf$ChrYcr), main="Ped female, chrY CR", ylim=c(0,1))
-} else {
-  plot.new()
-   plot.new()
-}
-
+#chrX call rate vs. chrY call rate
+plot(x$ChrXcr, x$ChrYcr, main=paste(infile, ": All samples"), pch=x$PedSex+1, 
+  col=x$PedSex+1, xlab="ChrX call rate", ylab="ChrY call rate")
 dev.off()
 
-#sex inference based on X het
-ChrXsex<-rep(0, nrow(x))
-ChrYsex<-rep(0, nrow(x))
-y<-cbind(x, ChrXsex, ChrYsex)
-y<-y[order(y$ChrXhetRate),]
-d<-diff(y$ChrXhetRate)
-w<-which.max(d)  #find the location of the biggest gap
+### estimate counts ###
+#chrX
+nx<-rep(NA, nrow(x))
+sel<-x$ChrXhetRate>0.1 & x$ChrXhetRate<0.3  #two X chr if 0.1<chrX(het)<0.3
+nx[sel]<-2
+sel<-x$ChrXhetRate<0.05   #one X chr if chrX(het)<0.05
+nx[sel]<-1
+sel<-x$ChrXhetRate>0.3   #three X chr if chrX(het)>0.3  ###adjust threshold?
+nx[sel]<-3
+#rest are NA
 
-y$ChrXsex[1:w]<-1  #samples with low chrXhet are male
-y$ChrXsex[(w+1):nrow(y)]<-2   #samples with high chrXhet are female
+#chrY
+ny<-rep(NA, nrow(x))
+sel<-x$ChrYcr>0.9   #one Y chr if chrY(cr)>0.9
+ny[sel]<-1
+sel<-x$ChrYcr<0.4   #no Y chr if chrY(cr)<0.4  (chip-dependent)
+ny[sel]<-0
+#rest are NA
+
+
+### inferred sex ###
+inf.sex<-rep(NA, nrow(x))
+sel<-nx==1 & ny==1   #1 X, 1 Y (male)
+inf.sex[sel]<-1
+sel<-nx==2 & ny==0   #2 X, 0 Y (female)
+inf.sex[sel]<-2
+
+
+#write it out, without doing anything about conflict with input sex/gender
+write.table(cbind(x$FID, x$IID, x$PedSex, inf.sex, nx, ny), outfile, row.names=F,
+  col.names=c("FID", "IID", "PedSex", "InferredSex", "NchrX", "NchrY"),
+  quote=F, sep="\t")
+
+#par(mfcol=c(2,3))
+
+#pu<-x[x$PedSex==0,]
+#pm<-x[x$PedSex==1,]
+#pf<-x[x$PedSex==2,]
+
+#if ( nrow(pu) > 0 ) {
+#  plot(sort(pu$ChrXhetRate), main="Ped unknown, chrX het", ylim=c(0,0.5))
+#  plot(sort(pu$ChrYcr), main="Ped unknown, chrY CR", ylim=c(0,1))
+#} else {
+#  plot.new()
+#  plot.new()
+#}
+
+#if ( nrow(pm) > 0 ) {
+#  plot(sort(pm$ChrXhetRate), main="Ped male, chrX het", ylim=c(0,0.5))
+#  plot(sort(pm$ChrYcr), main="Ped male, chrY CR", ylim=c(0,1))
+#} else {
+#   plot.new()
+#   plot.new()
+#}
+
+#if ( nrow(pf) > 0 ) {
+#  plot(sort(pf$ChrXhetRate), main="Ped female, chrX het", ylim=c(0,0.5))
+#  plot(sort(pf$ChrYcr), main="Ped female, chrY CR", ylim=c(0,1))
+#} else {
+#  plot.new()
+#   plot.new()
+#}
+
+#dev.off()
+
+#sex inference based on X het
+#ChrXsex<-rep(0, nrow(x))
+#ChrYsex<-rep(0, nrow(x))
+#y<-cbind(x, ChrXsex, ChrYsex)
+#y<-y[order(y$ChrXhetRate),]
+#d<-diff(y$ChrXhetRate)
+#w<-which.max(d)  #find the location of the biggest gap
+
+#y$ChrXsex[1:w]<-1  #samples with low chrXhet are male
+#y$ChrXsex[(w+1):nrow(y)]<-2   #samples with high chrXhet are female
 
 
 #sex inference based on Y cr
-y<-y[order(y$ChrYcr),]
-d<-diff(y$ChrYcr)
-w<-which.max(d)
-y$ChrYsex[1:w]<-2   #samples with low chrYcr are female
-y$ChrYsex[(w+1):nrow(y)]<-1
+#y<-y[order(y$ChrYcr),]
+#d<-diff(y$ChrYcr)
+#w<-which.max(d)
+#y$ChrYsex[1:w]<-2   #samples with low chrYcr are female
+#y$ChrYsex[(w+1):nrow(y)]<-1
 
 #make overall inference
-InferredSex<-y$ChrXsex
-sel<-y$ChrYsex != y$ChrXsex   #chrX and Y inference conflict
-InferredSex[sel]<-0   #set these to missing
+#InferredSex<-y$ChrXsex
+#sel<-y$ChrYsex != y$ChrXsex   #chrX and Y inference conflict
+#InferredSex[sel]<-0   #set these to missing
 
-sel<-y$PedSex != 0 & y$ChrXsex != y$PedSex   #pedsex differs from chr x sex
-InferredSex[sel]<-0   #set these to missing
+#sel<-y$PedSex != 0 & y$ChrXsex != y$PedSex   #pedsex differs from chr x sex
+#InferredSex[sel]<-0   #set these to missing
 
-write.table(cbind(y$FID, y$IID, y$PedSex, InferredSex, y$ChrXsex, y$ChrYsex),
-  outfile, row.names=F, col.names=c("FID", "IID", "PedSex",
-  "InferredSex", "ChrXsex", "ChrYsex"), quote=F, sep="\t")
+#write.table(cbind(y$FID, y$IID, y$PedSex, InferredSex, y$ChrXsex, y$ChrYsex),
+#  outfile, row.names=F, col.names=c("FID", "IID", "PedSex",
+#  "InferredSex", "ChrXsex", "ChrYsex"), quote=F, sep="\t")
